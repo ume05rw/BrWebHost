e670da81692a1c4e5515066a1c3a9ca08392c076
ume05rw <ume05rw@gmail.com>
2018-10-28 22:38:34 +0900

    イベントの実行中に破棄されることがあるため、イベント処理中にDBCを取得するように。

--- BroadlinkWeb/Models/Stores/RemoteHostStore.cs
+++ BroadlinkWeb/Models/Stores/RemoteHostStore.cs
@@ -127,54 +127,13 @@ namespace BroadlinkWeb.Models.Stores
             this._dbc = dbc;
         }
 
-
-
         public Task<IEnumerable<RemoteHost>> Refresh()
         {
             using (var cs = new Xb.Net.Udp())
             {
                 var packet = Encoding.UTF8.GetBytes(RemoteHostStore.CallString);
 
-                cs.OnRecieved += (object sender, Xb.Net.RemoteData rdata) =>
-                {
-                    var response = Encoding.UTF8.GetString(rdata.Bytes);
-                    if (response != RemoteHostStore.ResponseString)
-                        return;
-
-                    // 既存登録と合致照会、新規なら追加登録。
-                    var addrBytes = rdata.RemoteEndPoint.Address.GetAddressBytes();
-
-                    // IPv4射影アドレスのとき、v4アドレスに変換。
-                    if (
-                        // 長さが16バイト
-                        addrBytes.Length == 16
-                        // 先頭10バイトが全て0
-                        && addrBytes.Take(10).All(b => b == 0)
-                        // 11, 12バイトが FF
-                        && addrBytes.Skip(10).Take(2).All(b => b == 255)
-                    )
-                    {
-                        addrBytes = addrBytes.Skip(12).Take(4).ToArray();
-                    }
-                    var ipStr = (new IPAddress(addrBytes)).ToString();
-
-                    var exists = this._dbc.RemoteHosts
-                        .FirstOrDefault(r => r.IpAddressString == ipStr);
-
-                    // DB上に既に登録があるとき、何もしない。
-                    if (exists != null)
-                        return;
-
-                    // DB上に無いとき、新規レコードを追加。
-                    var entity = new RemoteHost()
-                    {
-                        Name = "Remote Broadlink-Web",
-                        IpAddressString = ipStr
-                    };
-
-                    this._dbc.Add(entity);
-                    this._dbc.SaveChanges();
-                };
+                cs.OnRecieved += this.OnResponseRecieved;
 
                 cs.SendTo(packet, IPAddress.Broadcast, RemoteHostStore.AckUdpPort);
 
@@ -195,9 +154,56 @@ namespace BroadlinkWeb.Models.Stores
                 }).ConfigureAwait(false)
                     .GetAwaiter()
                     .GetResult();
+
+                cs.OnRecieved -= this.OnResponseRecieved;
             }
 
             return null;
+        }
+
+        private void OnResponseRecieved(object sender, Xb.Net.RemoteData rdata)
+        {
+            var response = Encoding.UTF8.GetString(rdata.Bytes);
+            if (response != RemoteHostStore.ResponseString)
+                return;
+
+            // 既存登録と合致照会、新規なら追加登録。
+            var addrBytes = rdata.RemoteEndPoint.Address.GetAddressBytes();
+
+            // IPv4射影アドレスのとき、v4アドレスに変換。
+            if (
+                // 長さが16バイト
+                addrBytes.Length == 16
+                // 先頭10バイトが全て0
+                && addrBytes.Take(10).All(b => b == 0)
+                // 11, 12バイトが FF
+                && addrBytes.Skip(10).Take(2).All(b => b == 255)
+            )
+            {
+                addrBytes = addrBytes.Skip(12).Take(4).ToArray();
+            }
+            var ipStr = (new IPAddress(addrBytes)).ToString();
+
+            using (var serviceScope = RemoteHostStore.Provider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var dbc = serviceScope.ServiceProvider.GetService<Dbc>();
+                var exists = dbc.RemoteHosts
+                    .FirstOrDefault(r => r.IpAddressString == ipStr);
+
+                // DB上に既に登録があるとき、何もしない。
+                if (exists != null)
+                    return;
+
+                // DB上に無いとき、新規レコードを追加。
+                var entity = new RemoteHost()
+                {
+                    Name = "Remote Broadlink-Web",
+                    IpAddressString = ipStr
+                };
+
+                dbc.Add(entity);
+                dbc.SaveChanges();
+            }
         }
     }
 }
