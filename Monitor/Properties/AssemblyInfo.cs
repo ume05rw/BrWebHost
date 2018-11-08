cbcfd2eb15048f4c0f912411542d716d18d80aae
ume05rw <ume05rw@gmail.com>
2018-11-08 18:47:16 +0900

    モニタプログラム、バイナリ情報を書き込んでおく。

--- Monitor/Properties/AssemblyInfo.cs
+++ Monitor/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("Monitor")]
+[assembly: AssemblyTitle("BrWebHost Monitor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Monitor")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Do-Be's")]
+[assembly: AssemblyProduct("BrWebHost Monitor")]
+[assembly: AssemblyCopyright("Copyright Do-Be's©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
