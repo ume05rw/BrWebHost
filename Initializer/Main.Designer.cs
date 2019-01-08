07e90443e0d180f0853348cf1121535078f5e819
ume05rw <ume05rw@gmail.com>
2019-01-08 21:33:47 +0900

    デバイス初期化ツール用プロジェクトを追加。中身はこれから実装する。

---
+++ Initializer/Main.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Initializer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSsid = new System.Windows.Forms.Label();
+            this.txtSsid = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.chkPassword = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lblSsid
+            // 
+            this.lblSsid.AutoSize = true;
+            this.lblSsid.Location = new System.Drawing.Point(71, 31);
+            this.lblSsid.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblSsid.Name = "lblSsid";
+            this.lblSsid.Size = new System.Drawing.Size(64, 32);
+            this.lblSsid.TabIndex = 0;
+            this.lblSsid.Text = "SSID";
+            // 
+            // txtSsid
+            // 
+            this.txtSsid.Location = new System.Drawing.Point(145, 28);
+            this.txtSsid.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtSsid.Name = "txtSsid";
+            this.txtSsid.Size = new System.Drawing.Size(422, 38);
+            this.txtSsid.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(145, 83);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(422, 38);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(22, 86);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(113, 32);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password";
+            // 
+            // chkPassword
+            // 
+            this.chkPassword.AutoSize = true;
+            this.chkPassword.Location = new System.Drawing.Point(145, 130);
+            this.chkPassword.Name = "chkPassword";
+            this.chkPassword.Size = new System.Drawing.Size(200, 36);
+            this.chkPassword.TabIndex = 4;
+            this.chkPassword.Text = "Show Password";
+            this.chkPassword.UseVisualStyleBackColor = true;
+            this.chkPassword.CheckedChanged += new System.EventHandler(this.chkPassword_CheckedChanged);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 237);
+            this.Controls.Add(this.chkPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtSsid);
+            this.Controls.Add(this.lblSsid);
+            this.Font = new System.Drawing.Font("Yu Gothic UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Text = "Br Device Initializer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSsid;
+        private System.Windows.Forms.TextBox txtSsid;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.CheckBox chkPassword;
+    }
+}
+
---
+++ Initializer/Main.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Initializer
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            this.LoadSettings();
+        }
+
+        private void chkPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            this.txtPassword.PasswordChar = (this.chkPassword.Checked)
+                ? '\0'
+                : '*';
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.SaveSettings();
+        }
+
+        private void LoadSettings()
+        {
+            this.txtSsid.Text = Properties.Settings.Default.Ssid;
+            this.txtPassword.Text = Properties.Settings.Default.Password;
+        }
+
+        private void SaveSettings()
+        {
+            Properties.Settings.Default.Ssid = this.txtSsid.Text;
+            Properties.Settings.Default.Password = this.txtPassword.Text;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
